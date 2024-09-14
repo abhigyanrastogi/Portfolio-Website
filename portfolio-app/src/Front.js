cde99bc479f554dee7776f285b403eda303ec025
abhigyanrastogi <abhigyanrastogi0@gmail.com>
2024-09-14 22:00:46 +0530

    Front Page (Mobile safe)

--- portfolio-app/src/Front.js
+++ portfolio-app/src/Front.js
@@ -6,17 +6,17 @@ const Front = ({setGuest, setUser}) => {
         <div className='Front'>
             <header>
                 <svg width="100vw" height="40vh" xmlns="http://www.w3.org/2000/svg">
-                    <text x="0" y="32vh" fill="white" font-size="40vh">HI!</text>
+                    <text x="0" y="32vh" fill="white" fontSize="40vh">HI!</text>
                 </svg>
-                <svg width="100vw" height="15vh" xmlns="http://www.w3.org/2000/svg">
-                    <text x="20" y="11vh" fill="white" font-size="6.7vw">I am Abhigyan Chandra Rastogi</text>
+                <svg width="100vw" height="20vh" xmlns="http://www.w3.org/2000/svg">
+                    <text textAnchor="middle" x="50%" y="75%" fill="white" fontSize="7vw">I am Abhigyan Chandra Rastogi</text>
                 </svg>
             </header>
             <div className='Visit'>
                 <h1>Visit my website: </h1>
                 <div className='Buttons'>
-                    <button onClick={()=>{setGuest(true)}}>As a Guest</button>
-                    <button onClick={()=>{setUser(true)}}>As a User</button>
+                    <button className='visitButton' onClick={()=>{setGuest(true)}}>As a Guest</button>
+                    <button className='visitButton' onClick={()=>{setUser(true)}}>As a User</button>
                 </div>
             </div>
         </div>
